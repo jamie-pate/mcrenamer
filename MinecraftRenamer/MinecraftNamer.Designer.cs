3d52a0f7736629360153bb765c46b9f28afb5037
Jamie Pate <jpate@moscore.com>
2021-01-30 11:42:01 -0800

    initial commit

---
+++ MinecraftRenamer/MinecraftNamer.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace MCRenamer {
+    partial class MinecraftNamer {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MinecraftNamer));
+            this.namesBox = new System.Windows.Forms.ListBox();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.renameButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.reLaunchButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // namesBox
+            // 
+            this.namesBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.namesBox.FormattingEnabled = true;
+            this.namesBox.ItemHeight = 31;
+            this.namesBox.Location = new System.Drawing.Point(0, 70);
+            this.namesBox.Margin = new System.Windows.Forms.Padding(8);
+            this.namesBox.Name = "namesBox";
+            this.namesBox.Size = new System.Drawing.Size(1037, 357);
+            this.namesBox.TabIndex = 0;
+            this.namesBox.SelectedValueChanged += new System.EventHandler(this.namesBox_SelectedValueChanged);
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(8, 8);
+            this.nameBox.Margin = new System.Windows.Forms.Padding(8);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(373, 38);
+            this.nameBox.TabIndex = 1;
+            this.nameBox.TextChanged += new System.EventHandler(this.nameBox_TextChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.nameBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.removeButton, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.renameButton, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.addButton, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(8);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1037, 70);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // removeButton
+            // 
+            this.removeButton.Enabled = false;
+            this.removeButton.Location = new System.Drawing.Point(613, 8);
+            this.removeButton.Margin = new System.Windows.Forms.Padding(8);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(200, 54);
+            this.removeButton.TabIndex = 3;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // renameButton
+            // 
+            this.renameButton.Enabled = false;
+            this.renameButton.Location = new System.Drawing.Point(829, 8);
+            this.renameButton.Margin = new System.Windows.Forms.Padding(8);
+            this.renameButton.Name = "renameButton";
+            this.renameButton.Size = new System.Drawing.Size(200, 54);
+            this.renameButton.TabIndex = 4;
+            this.renameButton.Text = "Rename";
+            this.renameButton.UseVisualStyleBackColor = true;
+            this.renameButton.Click += new System.EventHandler(this.renameButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(397, 8);
+            this.addButton.Margin = new System.Windows.Forms.Padding(8);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(200, 54);
+            this.addButton.TabIndex = 2;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Controls.Add(this.reLaunchButton);
+            this.flowLayoutPanel1.Controls.Add(this.saveButton);
+            this.flowLayoutPanel1.Controls.Add(this.loadButton);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 427);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(8);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1037, 70);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(613, 8);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(8);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(200, 54);
+            this.saveButton.TabIndex = 0;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(397, 8);
+            this.loadButton.Margin = new System.Windows.Forms.Padding(8);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(200, 54);
+            this.loadButton.TabIndex = 1;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Visible = false;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // reLaunchButton
+            // 
+            this.reLaunchButton.Location = new System.Drawing.Point(829, 8);
+            this.reLaunchButton.Margin = new System.Windows.Forms.Padding(8);
+            this.reLaunchButton.Name = "reLaunchButton";
+            this.reLaunchButton.Size = new System.Drawing.Size(200, 54);
+            this.reLaunchButton.TabIndex = 2;
+            this.reLaunchButton.Text = "ReLaunch";
+            this.reLaunchButton.UseVisualStyleBackColor = true;
+            this.reLaunchButton.Click += new System.EventHandler(this.reLaunchButton_Click);
+            // 
+            // MinecraftNamer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1037, 497);
+            this.Controls.Add(this.namesBox);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(8);
+            this.Name = "MinecraftNamer";
+            this.Text = "Minecraft namer 2.0";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox namesBox;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Button renameButton;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button reLaunchButton;
+    }
+}
+
---
+++ MinecraftRenamer/MinecraftNamer.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.Management;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace MCRenamer {
+    public partial class MinecraftNamer : Form {
+
+        class ProfileHolder: INotifyPropertyChanged {
+            public ProfileHolder(dynamic profile) {
+                this.profile = profile;
+                PropertyChanged = null;
+            }
+            public ProfileHolder(String name) {
+                this.profile = new JProperty(Guid.NewGuid().ToString().Replace("-", ""), new JObject(new JProperty("displayName", name)));
+                PropertyChanged = null;
+            }
+            public dynamic profile;
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            public String name {
+                get { return profile.Value.displayName.Value; }
+                set {
+                    profile.Value.displayName.Value = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("name"));
+                }
+            }
+        }
+
+        class LauncherProfiles {
+            public dynamic jsonObj;
+            public dynamic profiles;
+            private String jsonPath;
+            public LauncherProfiles() {
+                String path = Environment.ExpandEnvironmentVariables("%APPDATA%\\.minecraft");
+                jsonPath = $"{path}\\launcher_profiles_namer.json";
+                profiles = new Newtonsoft.Json.Linq.JObject();
+                jsonObj = JsonConvert.DeserializeObject(File.ReadAllText(jsonPath)) as Newtonsoft.Json.Linq.JObject;
+                foreach (var p in jsonObj) {
+                    profiles[p.Name] = p.Value;
+                }
+            }
+
+            public void Save() {
+                jsonObj = profiles;
+                File.WriteAllText(jsonPath, JsonConvert.SerializeObject(jsonObj, Formatting.Indented));
+            }
+        }
+
+
+        BindingList<ProfileHolder> profiles = new BindingList<ProfileHolder>();
+
+        public MinecraftNamer() {
+            InitializeComponent();
+            LoadProfiles();
+        }
+
+        public void LoadProfiles() {
+            var lp = new LauncherProfiles();
+            namesBox.BeginUpdate();
+            profiles.Clear();
+            foreach(var p in lp.profiles) {
+                profiles.Add(new ProfileHolder(p));
+            }
+            namesBox.DisplayMember = "Name";
+            EnableDisable();
+            namesBox.DataSource = profiles;
+            namesBox.EndUpdate();
+        }
+
+        public void SaveProfiles() {
+            var lp = new LauncherProfiles();
+            (lp.profiles as JObject).RemoveAll();
+            foreach (ProfileHolder p in profiles) {
+                (lp.profiles as JObject).Add(p.profile);
+            }
+            lp.Save();
+        }
+
+        private void EnableDisable() {
+            var item = (ProfileHolder)namesBox.SelectedItem;
+            renameButton.Enabled = item != null && nameBox.Text.Length > 0 && nameBox.Text != item.name;
+            removeButton.Enabled = item != null && nameBox.Text.Length > 0;
+            bool dup = profiles.Any(p => p.name == nameBox.Text);
+            addButton.Enabled = nameBox.Text.Length > 0 && !dup;
+        }
+
+        private void namesBox_SelectedValueChanged(object sender, EventArgs e) {
+            if (namesBox.SelectedItem != null) {
+                nameBox.Text = ((ProfileHolder)namesBox.SelectedItem).name;
+            } else {
+                nameBox.Text = "";
+            }
+            EnableDisable();
+        }
+
+        private void addButton_Click(object sender, EventArgs e) {
+            dynamic item = new ProfileHolder(nameBox.Text);
+            profiles.Add(item);
+            namesBox.SelectedItem = item;
+            EnableDisable();
+        }
+
+        private void nameBox_TextChanged(object sender, EventArgs e) {
+            EnableDisable();
+        }
+
+        private void loadButton_Click(object sender, EventArgs e) {
+            LoadProfiles();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e) {
+            SaveProfiles();
+        }
+
+        private void removeButton_Click(object sender, EventArgs e) {
+            profiles.Remove((ProfileHolder)namesBox.SelectedItem);
+        }
+
+        private void renameButton_Click(object sender, EventArgs e) {
+            ProfileHolder ph = (ProfileHolder)namesBox.SelectedItem;
+            ph.name = nameBox.Text;
+
+            EnableDisable();
+        }
+
+        private List<String> jlpDirs = new List<String>();
+
+        private void reLaunchButton_Click(object sender, EventArgs e) {
+            var procs = Process.GetProcesses();
+            var mclProcs = procs.Where((p) => p.ProcessName == "MinecraftLauncher");
+
+            foreach (var process in procs) {
+                if (process.ProcessName == "javaw") {
+                    try {
+                        var cl = GetCommandLine(process);
+                        var usernameExpr = new Regex(@"--username [^ ]+");
+                        var gameDirExpr = new Regex("--gameDir (?:\"([^\"]+)\" +|([^ ]+) )");
+                        var jlpExpr = new Regex("(?:-Djava.library.path=\"([^\"]+)\"|-Djava.library.path=([^ ]+))");
+                        
+                        var firstArgExpr = new Regex("^(?:\"([^\"]+)\" +|([^ ]+) )");
+                        if (cl != null) {
+                            var newCl = firstArgExpr.Replace(usernameExpr.Replace(cl, "--username max"), "");
+                            var gameDir = gameDirExpr.Match(cl);
+                            var jlp = jlpExpr.Match(cl);
+                            var si = process.StartInfo;
+                            si.Arguments = newCl;
+                            var m = firstArgExpr.Match(cl);
+                            si.FileName = m.Groups[1].Value;
+                            if (si.FileName == "") {
+                                si.FileName = m.Groups[2].Value;
+                            }
+                            si.WorkingDirectory = gameDir.Groups[1].Value;
+                            if (si.WorkingDirectory == "") {
+                                si.WorkingDirectory = gameDir.Groups[2].Value;
+                            }
+                            var jlpDir = jlp.Groups[1].Value;
+                            if (jlpDir == "") {
+                                jlpDir = jlp.Groups[2].Value;
+                            }
+                            if (jlpDir != "") {
+                                jlpDirs.Add(jlpDir);
+                            }
+                            try {
+                                var proc = Process.Start(si);
+                                proc.EnableRaisingEvents = true;
+                                proc.Exited += Proc_Exited;
+                                proc.WaitForInputIdle(120000);
+                                if (proc.WaitForExit(1000)) {
+                                    System.Diagnostics.Debug.WriteLine(proc.ExitCode);
+                                } else {
+                                    process.Kill();
+                                    foreach (var mclp in mclProcs) {
+                                        mclp.Kill();
+                                    }
+                                }
+                            } catch (Exception ex) {
+                                 System.Diagnostics.Debug.WriteLine(ex.Message);
+                            }
+                            System.Diagnostics.Debug.WriteLine(newCl);
+                        }
+                    } catch (Win32Exception ex) when ((uint)ex.ErrorCode == 0x80004005) {
+                        // Intentionally empty - no security access to the process.
+                    } catch (InvalidOperationException) {
+                        // Intentionally empty - the process exited before getting details.
+                    }
+                }
+
+            }
+        }
+
+        private void Proc_Exited(object sender, EventArgs e) {
+            var proc = sender as Process;
+            proc.Exited -= Proc_Exited;
+            var remaining = new List<String>();
+            foreach(var d in jlpDirs) {
+                try {
+                    Directory.Delete(d, true);
+                } catch (Exception ex) {
+                    System.Diagnostics.Debug.WriteLine(ex.Message);
+                    remaining.Add(d);
+                }
+            }
+            jlpDirs = remaining;
+        }
+
+        private static string GetCommandLine(Process process) {
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT CommandLine FROM Win32_Process WHERE ProcessId = " + process.Id)) {
+                using (ManagementObjectCollection objects = searcher.Get()) {
+                    return objects.Cast<ManagementBaseObject>().SingleOrDefault()?["CommandLine"]?.ToString();
+                }
+            }
+        }
+    }
+}
